aa6cf19bdf7db33b0c2bea220a41b0b5f2cb237c
heleven-pc <heleven-pc@192.168.22.11>
2022-01-18 23:27:11 +0100

    get y set fecha hora

--- src/main/java/org/iesalandalus/programacion/citasclinica/Cita.java
+++ src/main/java/org/iesalandalus/programacion/citasclinica/Cita.java
@@ -3,7 +3,19 @@ package org.iesalandalus.programacion.citasclinica;
 import java.time.LocalDateTime;
 
 public class Cita {
-	public static final String FORMATO_FECHA_HORA="";
+	public static final String FORMATO_FECHA_HORA="dd/MM/yyyy HH:mm";
 	private LocalDateTime fechaHora;
+	
+	public LocalDateTime getFechaHora() {
+		return fechaHora;
+	}
+	
+	public void setFechaHora(LocalDateTime fechaHora) {
+		if (fechaHora == null)
+			throw new NullPointerException("ERROR: La fecha es nula.");
+		
+		this.fechaHora = fechaHora;
+	}
+	
 	
 }
