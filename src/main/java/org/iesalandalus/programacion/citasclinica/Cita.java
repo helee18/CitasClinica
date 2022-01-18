5fb42568d6e1cb669603a72c36c555db350e4549
heleven-pc <heleven-pc@192.168.22.11>
2022-01-18 22:25:15 +0100

    atributos clase cita

---
+++ src/main/java/org/iesalandalus/programacion/citasclinica/Cita.java
@@ -0,0 +1,9 @@
+package org.iesalandalus.programacion.citasclinica;
+
+import java.time.LocalDateTime;
+
+public class Cita {
+	public static final String FORMATO_FECHA_HORA="";
+	private LocalDateTime fechaHora;
+	
+}
