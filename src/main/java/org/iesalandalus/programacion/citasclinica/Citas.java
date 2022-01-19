8389885ce1e01f5bfbe90f3dd9a646d3bd4f97ca
heleven-pc <heleven-pc@192.168.22.11>
2022-01-19 21:18:09 +0100

    atributos + getters

--- src/main/java/org/iesalandalus/programacion/citasclinica/Citas.java
+++ src/main/java/org/iesalandalus/programacion/citasclinica/Citas.java
@@ -1,5 +1,15 @@
 package org.iesalandalus.programacion.citasclinica;
 
 public class Citas {
+	private int capacidad, tamaño;
+
+	public int getCapacidad() {
+		return capacidad;
+	}
+
+	public int getTamaño() {
+		return tamaño;
+	}
+	
 	
 }
