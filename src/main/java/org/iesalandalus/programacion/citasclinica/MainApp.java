c1d952a9c060fb89fc8e230e52c08688b2f48457
heleven-pc <heleven-pc@192.168.22.11>
2022-01-20 17:50:31 +0100

    metodo insertar cita

--- src/main/java/org/iesalandalus/programacion/citasclinica/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/citasclinica/MainApp.java
@@ -1,9 +1,43 @@
 package org.iesalandalus.programacion.citasclinica;
 
+import javax.naming.OperationNotSupportedException;
+
+import org.iesalandalus.programacion.citasclinica.modelo.Cita;
+import org.iesalandalus.programacion.citasclinica.modelo.Citas;
+import org.iesalandalus.programacion.citasclinica.vista.Consola;
+
 public class MainApp {
+	
+	public static final int NUM_MAX_CITAS = 10;
+	private static Citas listaCitas = new Citas(NUM_MAX_CITAS);
 
 	public static void main(String[] args) {
-		System.out.println("Programa para gestionar las citas de la Clínica.");
+		int opcion=0;
+		
+		do {
+			System.out.println("Programa para gestionar las citas de la Clínica.");
+			Consola.mostrarMenu();
+			
+			//opcion = elegirOpcion();
+			//ejecutarOpcion(opcion);
+			
+		} while (opcion >= 0 && opcion >= 5);
+	}
+	
+	private void insertarCita () {
+		try {
+			// creamos una cita
+			Cita cita = Consola.leerCita();
+			
+			// la añadimos al array de citas
+			Citas citas = new Citas(NUM_MAX_CITAS);
+			citas.insertar(cita);
+			
+			System.out.println(" Cita asignada correctamente.");
+			
+		} catch (IllegalArgumentException | OperationNotSupportedException | NullPointerException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 	
 }
