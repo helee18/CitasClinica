fb00e5aa111ad896ac4b8c88d83de050f20e928c
heleven-pc <heleven-pc@192.168.22.11>
2022-01-20 01:08:08 +0100

    metedo elegir opcion

--- src/main/java/org/iesalandalus/programacion/citasclinica/vista/Consola.java
+++ src/main/java/org/iesalandalus/programacion/citasclinica/vista/Consola.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.citasclinica.vista;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class Consola {
 	private Consola () {
 		
@@ -13,6 +15,21 @@ public class Consola {
 		System.out.println("3.- Borrar cita.");
 		System.out.println("4.- Mostrar citas por día.");
 		System.out.println("5.- Mostrar todas las citas.");
-		System.out.println("6.- Salir.");
+		System.out.println("0.- Salir.");
+	}
+	
+	public static Opciones elegirOpcion() 
+	{
+		// introducimos en un array las opciones
+		Opciones[] opcion = Opciones.values();
+		
+		do {
+			
+			System.out.println("Seleccione una opción entre 0 y 5:");
+			int opcionElegida = Entrada.entero();
+			
+		} while (opcionElegida < 0 || opcionElegida > 5) ;
+
+		return opcion[opcionElegida];
 	}
 }
