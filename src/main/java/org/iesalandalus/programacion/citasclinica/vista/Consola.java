fcba6951a9642de02565338dc8995f75affe437a
heleven-pc <heleven-pc@192.168.22.11>
2022-01-20 01:04:30 +0100

    metodo menu

--- src/main/java/org/iesalandalus/programacion/citasclinica/vista/Consola.java
+++ src/main/java/org/iesalandalus/programacion/citasclinica/vista/Consola.java
@@ -5,5 +5,14 @@ public class Consola {
 		
 	}
 	
-	
+	public static void mostrarMenu() {
+		System.out.println("Menú");
+		System.out.println("-----------------------------");
+		System.out.println("1.- Insertar cita.");
+		System.out.println("2.- Buscar cita.");
+		System.out.println("3.- Borrar cita.");
+		System.out.println("4.- Mostrar citas por día.");
+		System.out.println("5.- Mostrar todas las citas.");
+		System.out.println("6.- Salir.");
+	}
 }
