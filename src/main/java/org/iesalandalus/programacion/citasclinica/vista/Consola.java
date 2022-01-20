d9a4daa3182e2cda023bb2d42b961c655d040445
heleven-pc <heleven-pc@192.168.22.11>
2022-01-20 01:13:38 +0100

    metodo leer paciente

--- src/main/java/org/iesalandalus/programacion/citasclinica/vista/Consola.java
+++ src/main/java/org/iesalandalus/programacion/citasclinica/vista/Consola.java
@@ -1,5 +1,8 @@
 package org.iesalandalus.programacion.citasclinica.vista;
 
+import javax.naming.OperationNotSupportedException;
+
+import org.iesalandalus.programacion.citasclinica.modelo.Paciente;
 import org.iesalandalus.programacion.utilidades.Entrada;
 
 public class Consola {
@@ -18,18 +21,37 @@ public class Consola {
 		System.out.println("0.- Salir.");
 	}
 	
-	public static Opciones elegirOpcion() 
-	{
+	public static Opciones elegirOpcion() {
 		// introducimos en un array las opciones
 		Opciones[] opcion = Opciones.values();
+		
+		int opcionElegida;
 		
 		do {
 			
 			System.out.println("Seleccione una opción entre 0 y 5:");
-			int opcionElegida = Entrada.entero();
+			opcionElegida = Entrada.entero();
 			
 		} while (opcionElegida < 0 || opcionElegida > 5) ;
 
 		return opcion[opcionElegida];
 	}
+	
+	public static Paciente leerPaciente() throws OperationNotSupportedException {
+		Paciente paciente;
+		
+		System.out.println("Introduzca el nombre:");
+		String nombre = Entrada.cadena();
+		
+		System.out.println("Introduzca el teléfono:");
+		String telefono = Entrada.cadena();
+		
+		System.out.println("Introduzca el DNI:");
+		String dni = Entrada.cadena();
+		
+		paciente = new Paciente(nombre, dni, telefono);
+		
+		return paciente;
+	}
+	
 }
